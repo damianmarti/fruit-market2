ff7cbdd6f71823f41775572aa935068415763e4d
Damian <damianmarti@gmail.com>
2024-01-09 16:26:23 -0300

    Removed unused imports on Footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { hardhat } from "viem/chains";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
-import { useGlobalState } from "~~/services/store/store";
-import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 /**
  * Site footer
